4640c998721bf89a742ccda6e64d6e144805b5ed
jyothi8055 <nagajyothi2575@gmail.com>
2020-06-22 16:25:26 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { PropTypes } from 'react';
+import { StyleSheet, css } from 'aphrodite';
+import Header from './components/header';
+import Article from './components/article';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const styles = StyleSheet.create({
+  card: {
+    margin: '0',
+    padding: '0',
+    boxSizing: 'border-box',
+    width: '300px',
+    maxWidth: '300px',
+    height: 'auto',
+    backgroundColor: '#f4f4f4',
+    transition: 'all .3s ease'
+  },
 
-export default App;
+  hover: {
+    ':hover': {
+      boxShadow: '0px 0px 2px #ccc',
+      transform: 'scale(1.01,1.01)'
+    }
+  }
+});
+
+const Card = ({ src, titleSmallWord, titleBigWord, color, text, children}) => (
+  <div className={css(styles.card, styles.hover)}>
+    <Header
+      src={src}
+      titleSmallWord={titleSmallWord}
+      titleBigWord={titleBigWord}
+      color={color} />
+
+    <Article
+      text={children} />
+  </div>
+)
+
+Card.defaultProps = {
+  src: 'https://avatars.githubusercontent.com/u/9022134?v=3',
+  titleSmallWord: 'Hello',
+  titleBigWord: 'World',
+  color: 'purple',
+  text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+};
+
+Card.propTypes = {
+  src: PropTypes.string,
+  titleSmallWord: PropTypes.string,
+  titleBigWord: PropTypes.string,
+  color: PropTypes.string,
+  text: PropTypes.string,
+  children: PropTypes.node.isRequired
+};
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
+import Card from './C:/Users/naresh/Desktop/myproject/src/App.js';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => (
+  <div className='container'>
+    <Card
+      src='http://www.media.inaf.it/wp-content/uploads/2014/02/Einstein_laughing.jpeg'
+      titleSmallWord='Albert'
+      titleBigWord='Einstein'
+      color='tomato'>
+      Albert Einstein was a German-born theoretical physicist. He developed the general theory of relativity, one of the two pillars of modern physics (alongside quantum mechanics). Einstein's work is also known for its influence on the philosophy of science.
+    </Card>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    <Card
+      src='https://upload.wikimedia.org/wikipedia/commons/b/b9/Steve_Jobs_Headshot_2010-CROP.jpg'
+      titleSmallWord='Steve'
+      titleBigWord='Jobs'
+      color='yellow'>
+      Steven Paul 'Steve' Jobs was an American information technology entrepreneur and inventor. He was the co-founder, chairman, and chief executive officer (CEO) of Apple Inc.; CEO and majority shareholder of Pixar Animation Studios; a member of The Walt Disney Company's board of directors following its acquisition of Pixar; and founder, chairman, and CEO of NeXT Inc.
+    </Card>
+
+    <Card
+      src='http://images.uncyc.org/pt/0/04/Linus_Torvalds.jpg'
+      titleSmallWord='Linus'
+      titleBigWord='Torvalds'>
+      Linus Benedict Torvalds, born December 28, 1969, is a Finnish software engineer, American naturalized, who is the creator and, for a long time, principal developer, of the Linux kernel, which became the kernel for operating systems (and many distributions of each) such as GNU and years later Android and Chrome OS. He also created the distributed revision control system git.
+    </Card>
+  </div>
+)
+
+render(<App /> , document.getElementById('App'));
